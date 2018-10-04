6253f81703a2a22f05069c96d766b1716d3b4a9a
Walt Elam <wrelam@gmail.com>
2018-10-03 22:36:10 -0400

    Added hello world flask app

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # Load the instance config if it exists and we're not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config
+        app.config.from_pyfile(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Create a hello world page
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
